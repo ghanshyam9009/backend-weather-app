9c6f2403f505e2632a872100316680de22790b93
Ghanshyam Choudhary <147299599+ghanshyam9009@users.noreply.github.com>
2026-04-01 20:46:19 +0530

    update cors

--- server.js
+++ server.js
@@ -12,11 +12,27 @@ const PORT = process.env.PORT || 5000;
 // const CLIENT_URL = process.env.CLIENT_URL || "http://localhost:5173";
 const CLIENT_URL = process.env.CLIENT_URL || "https://frontend-weather-app-diyw.onrender.com";
 
-app.use(
-  cors({
-    origin: CLIENT_URL,
-  })
-);
+// app.use(
+//   cors({
+//     origin: CLIENT_URL,
+//   })
+// );
+
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://frontend-weather-app-diyw.onrender.com"
+];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  }
+}));
+
 app.use(express.json());
 
 connectDB();
