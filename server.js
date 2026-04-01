a8b88f20c8d936ad44cd54addc6da965c06892a2
Ghanshyam Choudhary <147299599+ghanshyam9009@users.noreply.github.com>
2026-04-01 20:36:34 +0530

    update frontend url

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 // const CLIENT_URL = process.env.CLIENT_URL || "http://localhost:5173";
-const CLIENT_URL = process.env.CLIENT_URL || "https://frontend-weather-app-diyw.onrender.com/";
+const CLIENT_URL = process.env.CLIENT_URL || "https://frontend-weather-app-diyw.onrender.com";
 
 app.use(
   cors({
